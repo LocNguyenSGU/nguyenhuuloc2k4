f86a353dcd6d75cf6d114c84027cd95d57db0824
LocNguyenSGU <lockbkbang@gmail.com>
2025-03-03 19:41:54 +0700

    first commit

---
+++ assets/js/dark.js
@@ -0,0 +1,39 @@
+const darkIcon = document.getElementById('dark-icon');
+const lightIcon = document.getElementById('light-icon');
+const themeToggleBtn = document.getElementById('theme-toggle');
+
+if (localStorage.getItem('color-theme') === 'dark' || document.documentElement.classList.contains('dark')) {
+    setDark();
+} else {
+    setLight();
+}
+
+themeToggleBtn.addEventListener('click', function () {
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            setDark();
+        } else {
+            setLight();
+        }
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            setLight();
+        } else {
+            setDark();
+        }
+    }
+});
+
+function setDark() {
+    document.documentElement.classList.add('dark');
+    localStorage.setItem('color-theme', 'dark');
+    lightIcon.classList.remove('hidden');
+    darkIcon.classList.add('hidden');
+}
+
+function setLight() {
+    document.documentElement.classList.remove('dark');
+    localStorage.setItem('color-theme', 'light');
+    darkIcon.classList.remove('hidden');
+    lightIcon.classList.add('hidden');
+}
---
+++ assets/js/typing.js
@@ -0,0 +1,39 @@
+let part = 0;
+let partIndex = 0;
+const content = [$('meta[name=author]').prop('content'), "@" + $('meta[name=github]').prop('content')];
+const typingObj = document.querySelector("#typing");
+let interval = setInterval(Typing, 100);
+
+function Typing() {
+    const text = content[part].substring(0, partIndex + 1);
+    typingObj.innerHTML = text;
+    partIndex++;
+
+    if (text === content[part]) {
+        clearInterval(interval);
+        setTimeout(function () {
+            interval = setInterval(Delete, 50);
+        }, 1000);
+    }
+}
+
+function Delete() {
+    const text = content[part].substring(0, partIndex - 1);
+    typingObj.innerHTML = text;
+    partIndex--;
+
+    if (text === '') {
+        clearInterval(interval);
+
+        if (part === (content.length - 1))
+            part = 0;
+        else
+            part++;
+
+        partIndex = 0;
+
+        setTimeout(function () {
+            interval = setInterval(Typing, 100);
+        }, 200);
+    }
+}
